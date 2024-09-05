55cf32bc7340a4fb677e4c876afb35995001188f
Hersheys6969 <harshzopem21@gmail.com>
2024-09-05 21:55:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const w = window.innerWidth;
 const h = window.innerHeight;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
-camera.position.y=25;
-camera.position.z = 50;
+camera.position.y=5;
+camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
