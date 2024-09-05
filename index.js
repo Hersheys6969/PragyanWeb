7f061c23ae248a1560ae8578e2455e67117cb214
Hersheys6969 <harshzopem21@gmail.com>
2024-09-05 21:08:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const material = new THREE.MeshPhongMaterial({
 // material.map.colorSpace = THREE.SRGBColorSpace;
 const earthMesh = new THREE.Mesh(geometry, material);
 earthGroup.add(earthMesh);
-earthGroup.position.x = -100;
+earthGroup.position.x = -50;
 
 const lightsMat = new THREE.MeshBasicMaterial({
   map: loader.load("./textures/03_earthlights1k.jpg"),
